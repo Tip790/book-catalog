048d6244b56e6b6c6515a69e2daeef6ce64d8f43
Tip790 <aartho1@buffs.wtamu.edu>
2026-09-13 22:38:02 -0500

    Set up Models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    status = models.CharField(max_length=20, default="not in print")
+    
+    def __str__(self):
+        return self.title, self.status
+    
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
